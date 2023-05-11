2b58607fcb2da9161853498f170032435a68b527
长吁短叹 <108162823+zghlin@users.noreply.github.com>
2023-05-11 20:40:52 +0800

    Add files via upload
    Signed-off-by: 长吁短叹 <108162823+zghlin@users.noreply.github.com>

---
+++ 字符串输入输出1.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main(void)
+{
+	
+	char str[]="http";
+	printf("%s",str);
+	printf("%s","http");
+	puts(str);
+	puts("http");
+	system("pause");
+	return 0;
+}
